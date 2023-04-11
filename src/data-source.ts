8439b22b3fdca5213998e8e547cd00724d4b4369
lVladimirl <infinitdungeun@gmail.com>
2023-04-11 11:29:39 -0300

    initial commit

--- src/data-source.ts
+++ src/data-source.ts
@@ -12,11 +12,11 @@ const AppDataSource = new DataSource(
     } :
     {
         type: "postgres",
-        host: process.env.PGHOST,
-        port: parseInt(process.env.PGPORT!),
-        username: process.env.PGUSER,
-        password: process.env.PGPASSWORD,
-        database: process.env.PGDATABASE,
+        host: process.env.DB_HOST,
+        port: 5432,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB,
         logging: true,
         synchronize: false,
         entities: [path.join(__dirname, "./entities/**.{js,ts}")],
