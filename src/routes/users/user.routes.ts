dad8eceeb5f8e4b970e1f66368bec0d08478714c
lVladimirl <infinitdungeun@gmail.com>
2023-04-24 14:52:55 -0300

    user delete route running and tests are ok

--- src/routes/users/user.routes.ts
+++ src/routes/users/user.routes.ts
@@ -5,11 +5,13 @@ import { isAdm } from "../../middlewares/isAdm.middleware";
 import userListController from "../../controllers/users/userList.controller";
 import userPatchController from "../../controllers/users/userPatch.controller";
 import { isAdmOrOwner } from "../../middlewares/isAdmOrOwner.middleware";
+import userDeleteController from "../../controllers/users/userDelete.controller";
 
 const userRoutes = Router();
 
 userRoutes.post("", userCreateController);
 userRoutes.get("", isLoged, isAdm, userListController);
 userRoutes.patch("/:id", isLoged, isAdmOrOwner, userPatchController);
+userRoutes.delete("/:id", isLoged, isAdm, userDeleteController);
 
 export default userRoutes;
